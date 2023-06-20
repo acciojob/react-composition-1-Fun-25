c4b5d63af418ad04e5f00fa2f784070e842fc152
Fun-25 <muskandodmani760@gmail.com>
2023-06-20 10:29:17 +0530

    submitting code-Tue Jun 20 2023 10:29:15 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
 
+import { useState } from 'react';
 import React from "react";
 import './../styles/App.css';
+
+const tabs = [
+  {
+    title: "Tab 1",
+    content: "This is the content for Tab 1",
+  },
+  {
+    title: "Tab 2",
+    content: "This is the content for Tab 2",
+  },
+  {
+    title: "Tab 3",
+    content: "This is the content for Tab 3",
+  }
+];
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+       <Tab tabs={tabs}/>
     </div>
   )
 }
 
-export default App
+const Tab = ({ tabs }) => {
+  const [tabstate, setTabstate] = useState(null);
+  function showContent(tab) {
+    setTabstate(tab);
+  }
+  return (
+    <div>
+      {tabs.map((tab) => (
+        <a>
+          <li key={tab.title} onClick={() => showContent(tab)}>
+            {tab.title}
+          </li>
+        </a>
+      ))}
+      {tabstate && <p>{tabstate.content}</p>}
+    </div>
+  );
+};
+
+ export default App;
+
