beb2b2b4acb9b7485840746971710e7e4c4b47ac
Fun-25 <muskandodmani760@gmail.com>
2023-06-20 11:48:25 +0530

    submitting code-Tue Jun 20 2023 11:48:22 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,19 @@
-
-import { useState } from 'react';
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Tabs from "./Tabs";
 
-const tabs = [
-  { title: "Tab 1", content: "This is the content for Tab 1." },
-  { title: "Tab 2", content: "This is the content for Tab 2." },
-  { title: "Tab 3", content: "This is the content for Tab 3." },
-];
+function App() {
+  const tabs = [
+    { title: "Tab 1", content: "This is the content for Tab 1." },
+    { title: "Tab 2", content: "This is the content for Tab 2." },
+    { title: "Tab 3", content: "This is the content for Tab 3." },
+  ];
 
-const App = () => {
   return (
     <div>
-       <Tab tabs={tabs}/>
-    </div>
-  )
-}
-
-const Tab = ({ tabs }) => {
-  const [tabstate, setTabstate] = useState(null);
-  function showContent(tab) {
-    setTabstate(tab);
-  }
-  return (
-    <div>
-      {tabs.map((tab) => (
-        <a>
-          <li key={tab.title} onClick={() => showContent(tab)}>
-            {tab.title}
-          </li>
-        </a>
-      ))}
-      {tabstate && <div>{tabstate.content}</div>}
+      <Tabs tabs={tabs} />
     </div>
   );
-};
+}
 
- export default App;
-
+export default App;
