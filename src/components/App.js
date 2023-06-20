4c6b15af2b6b018706b26daae543ab956706424c
Fun-25 <muskandodmani760@gmail.com>
2023-06-20 11:28:38 +0530

    submitting code-Tue Jun 20 2023 11:28:30 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,9 @@ import React from "react";
 import './../styles/App.css';
 
 const tabs = [
-  {
-    title: "Tab 1",
-    content: "This is the content",
-  },
-  {
-    title: "Tab 2",
-    content: "This is the content",
-  },
-  {
-    title: "Tab 3",
-    content: "This is the content",
-  }
+  { title: "Tab 1", content: "This is the content for Tab 1." },
+  { title: "Tab 2", content: "This is the content for Tab 2." },
+  { title: "Tab 3", content: "This is the content for Tab 3." },
 ];
 
 const App = () => {
